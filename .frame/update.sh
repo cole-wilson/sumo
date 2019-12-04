3fe1d8a27018475ab8f59c5da84b4aafaed5bcd4
cole-wilson <49500253+cole-wilson@users.noreply.github.com>
2019-12-04 07:51:51 -0800

    Update and rename .gitignore/update.sh to .frame/update.sh

--- .frame/update.sh
+++ .frame/update.sh
@@ -6,4 +6,12 @@ cd /home/robot
 echo "Cloning..."
 git clone https://github.com/cole-wilson/sumo.git
 cd sumo
+echo "Marking as executable..."
+chmod +x *.py
 echo "Updated 'sumo'"
+echo
+$run = "run() {echo 'running'$1; python3 '$1'}"
+
+$run >> ~/.bashrc
+
+echo "To run type `run *.py`"
