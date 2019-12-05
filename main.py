c1cc78874ef395ef0731e638ae830a726c8b4f5f
cole <geekmonster842@gmail.com>
2019-12-05 14:53:17 -0800

    p

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 # So program can be run from Brickman
-
 from ev3dev.ev3 import *
 from time import sleep
-
 input('enter to start')
 
-a = LargeMotor('outB')
+arm = LargeMotor('outB')
 print('Moving arm')
-a.run_timed(time_sp=1000, speed_sp=750)
+arm.run_timed(time_sp=200, speed_sp=999)
 
-m = LargeMotor('outC')
+wheel = LargeMotor('outC')
 print('sleeping')
 sleep(3)
 print('driving')
 
-m.run_timed(time_sp=3000, speed_sp=-999)
+wheel.run_timed(time_sp=3000, speed_sp=-999)
