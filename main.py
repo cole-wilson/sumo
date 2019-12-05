649441a005cd5014401175e9c37adb88945a6e70
cole <geekmonster842@gmail.com>
2019-12-05 14:18:05 -0800

    p

--- main.py
+++ main.py
@@ -4,9 +4,12 @@
 from ev3dev.ev3 import *
 from time import sleep
 a = LargeMotor('outB')
-a.run_timed(time_sp=1000, speed_sp=-750)
+print('Moving arm')
+a.run_timed(time_sp=1000, speed_sp=750)
 
 m = LargeMotor('outC')
+print('sleeping')
 sleep(3)
+print('driving')
 m.run_forever(speed_sp=900)
 m.stop(stop_action="hold")
