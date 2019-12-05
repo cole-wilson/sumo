c98f40347282136d492b5b04d0a3d093000d34a0
cole <geekmonster842@gmail.com>
2019-12-05 13:59:48 -0800

    p

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Load is EXTREMELY successful')
+#!/usr/bin/env python3
+# So program can be run from Brickman
+
+from ev3dev.ev3 import *
+from time import sleep
+m = LargeMotor('outB')
+
+m.run_forever(speed_sp=900)
+sleep(5)
+m.stop(stop_action="hold")
+sleep(5)
