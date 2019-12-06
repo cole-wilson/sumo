7e43a3e0613198f2e2541a32193842995ac07a3c
cole <geekmonster842@gmail.com>
2019-12-06 14:10:45 -0800

    Added TODO compatibility

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ while not(ts1.value() or ts2.value()) or (us.value()<100) :
     print('Touch Sensor 2 state:' + str(ts2.value()))
     print('Light Sensor: ' + colors[ls.value()])
     print('=', end='')
+    #
     for x in range(round(us.value()/2)):
         print(' ',end='')
     print('+')
