d141f731d4d57a56631a3cb5950069d6f97efdbf
cole <geekmonster842@gmail.com>
2019-12-05 16:05:41 -0800

    Added TODO compatibility

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 # So program can be run from Brickman
 from ev3dev.ev3 import *
 from time import sleep
-
-
 #TODO: update sensors on physical robot.
 
 # Set sensor classes:
@@ -21,11 +19,11 @@ print('Starting Program')
 input('Press the Enter key to start the program.')
 
 print('Moving arm for 200 milliseconds.')
-#arm.run_timed(time_sp=200, speed_sp=999)
+arm.run_timed(time_sp=200, speed_sp=999)
 sleep(0.75)
 print('Waiting for Touch Sensor, or Ultrasonic Sensor')
 while not(ts1.value() or ts2.value()) or (us.value()<100) :
     print('.',end='')
-#wheel.run_timed(time_sp=3000, speed_sp=-999)
+wheel.run_timed(time_sp=3000, speed_sp=-999)
 print('Done')
 input()
